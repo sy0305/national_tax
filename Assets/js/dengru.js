5edb53310298d8fd8953f810972b5656a05266d0
sunyong <260968736@qq.com>
2019-04-18 23:07:31 +0800

    工资条灯芯

---
+++ Assets/js/dengru.js
@@ -0,0 +1,11 @@
+
+
+     $("#f2").submit(function () {
+         $(".caidan").css("display","block");
+         $(".d1").text("欢迎xx");
+         $(".zhuce button").css("display","none");
+         console.log("提交成功")
+     })
+
+
+
